c0e33df68a5b2322e216689116f7d505951e6638
Oscar <ojimenezm4@miumg.edu.gt>
2024-05-31 23:30:27 -0600

    Comit5

--- index.js
+++ index.js
@@ -5,15 +5,25 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
 require("dotenv").config({ path: "variables.env" });
+
 // Conectar a MongoDB
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB conectado");
+  })
+  .catch((error) => {
+    console.error("Error conectando a MongoDB:", error.message);
+    process.exit(1); // Salir si hay un error de conexión
+  });
 
 // Crear el servidor
 const app = express();
+
 // Configurar carpeta pública para archivos subidos
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
@@ -22,32 +32,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const whitelist = [process.env.FRONTEND_URL];
-
 const corsOptions = {
   origin: (origin, callback) => {
-    // Permitir solicitudes sin origen (como las de recursos estáticos)
-    if (!origin) {
-      return callback(null, true);
-    }
-
-    if (whitelist.includes(origin)) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("No permitido por CORS"));
     }
   },
 };
-
-// Habilitar CORS con las opciones configuradas
 app.use(cors(corsOptions));
 
 // Rutas de la app
 app.use("/", routes());
 
-//esto se hace siempre para desplegar los proyectos en heroku
+// Configurar puerto y host
 const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 5000;
-// iniciarAPP
 app.listen(port, host, () => {
-  console.log("El servidor está funcionando ");
+  console.log(`El servidor está funcionando en http://${host}:${port}`);
 });
